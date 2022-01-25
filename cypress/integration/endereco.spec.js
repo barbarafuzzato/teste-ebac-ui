c3eabf7d78a1b201d33fa4efedb28fa8d9a1495f
Bárbara <barbarafuzzato@gmail.com>
2022-01-25 20:19:13 -0300

    Page Objects, Massa de Dados em Lista e Relatórios.

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,7 +12,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+        EnderecoPage.editarEnderecoFaturamento('Betina', 'Mendes', 'Apple', 'Brasil', 'Avenida Quinze', '456', 'Ilhabela', 'São Paulo', '10100-100', '99999999999', 'email@dominio.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo  de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
+    });
+
 });
